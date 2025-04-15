633f258b69c88486c9060fca4753906c70c5ef83
Kathir Karthik  M <kathirkarthik001@gmail.com>
2025-04-15 12:41:00 +0530

    add : db-connect test

--- server.js
+++ server.js
@@ -3,8 +3,14 @@ const userRoutes = require('./route/user');
 const docRoutes = require('./route/doc');
 const app = express();
 const port = 3000;
+const pool = require('./config/db');
 
 app.use(express.json());
+
+//  DB connection Check 
+pool.query('SELECT NOW()')
+    .then(res => console.log('DB Connected:', res.rows[0]))
+    .catch(err => console.error('DB Connection Failed:', err.message));
 
 // Routes
 app.use('/api/user', userRoutes);
